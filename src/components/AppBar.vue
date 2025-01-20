dcea91415efee5ff782ce14c999aee96750caffa
Marylynn <marlymaingi28@gmail.com>
2025-01-20 13:03:11 +0300

    initial commit

---
+++ src/components/AppBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-navigation-drawer color="primary" rail="rail">
+      <v-list nav>
+        <v-list-item prepend-icon="mdi-view-dashboard"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-box"></v-list-item>
+        <v-list-item prepend-icon="mdi-wallet-bifold"></v-list-item>
+        <v-list-item prepend-icon="mdi-cash"></v-list-item>
+        <v-list-item prepend-icon="mdi-bullseye-arrow"></v-list-item>
+        <v-list-item prepend-icon="mdi-account"></v-list-item>
+        <v-list-item prepend-icon="mdi-finance"></v-list-item>
+        <v-list-item prepend-icon="mdi-face-agent"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  
+    <v-app-bar flat>
+      <v-app-bar-title>
+        <v-icon icon="mdi-circle-slice-6" />
+  
+        Essentials Preset
+      </v-app-bar-title>
+    </v-app-bar>
+  </template>
+  
+  <script setup>
+  //
+  </script>
+  
---
+++ src/components/dashboard/Chart.vue
@@ -0,0 +1,46 @@
+<template>
+  <Line :data="data" :options="chartOptions" />
+</template>
+
+<script setup>
+import { Line } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement, // Register PointElement
+  LineElement, // Register LineElement
+} from "chart.js";
+
+// Register all necessary components
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement
+);
+
+const labels = [1, 2, 3, 4, 5, 6, 7];
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: "rgb(75, 192, 192)",
+      tension: 0.1,
+    },
+  ],
+};
+
+const chartOptions = reactive({
+  responsive: true,
+});
+</script>
