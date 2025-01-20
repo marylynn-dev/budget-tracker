38223153db0e0e320f0e7c93a039394a7d0b8e42
Marylynn <marlymaingi28@gmail.com>
2025-01-20 14:31:00 +0300

    initial

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,27 +1,45 @@
 <template>
-    <v-navigation-drawer color="primary" rail="rail">
-      <v-list nav>
-        <v-list-item prepend-icon="mdi-view-dashboard"></v-list-item>
-        <v-list-item prepend-icon="mdi-account-box"></v-list-item>
-        <v-list-item prepend-icon="mdi-wallet-bifold"></v-list-item>
-        <v-list-item prepend-icon="mdi-cash"></v-list-item>
-        <v-list-item prepend-icon="mdi-bullseye-arrow"></v-list-item>
-        <v-list-item prepend-icon="mdi-account"></v-list-item>
-        <v-list-item prepend-icon="mdi-finance"></v-list-item>
-        <v-list-item prepend-icon="mdi-face-agent"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  
-    <v-app-bar flat>
-      <v-app-bar-title>
-        <v-icon icon="mdi-circle-slice-6" />
-  
-        Essentials Preset
-      </v-app-bar-title>
-    </v-app-bar>
-  </template>
-  
-  <script setup>
-  //
-  </script>
-  
+  <v-navigation-drawer color="primary" rail="rail">
+    <v-list nav>
+      <v-list-item
+        prepend-icon="mdi-view-dashboard"
+        to="/dashboard"
+      ></v-list-item>
+      <v-list-item prepend-icon="mdi-account-box"></v-list-item>
+      <v-list-item prepend-icon="mdi-wallet-bifold"></v-list-item>
+      <v-list-item prepend-icon="mdi-cash"></v-list-item>
+      <v-list-item prepend-icon="mdi-bullseye-arrow"></v-list-item>
+      <v-list-item prepend-icon="mdi-account"></v-list-item>
+      <v-list-item prepend-icon="mdi-finance"></v-list-item>
+      <v-list-item prepend-icon="mdi-face-agent"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+
+  <v-app-bar flat>
+    <v-row>
+      <v-col cols="12" md="8" lg="4">
+        <v-text-field
+          append-inner-icon="mdi-magnify"
+          density="compact"
+          variant="solo"
+          placeholder="Search Here"
+        >
+        </v-text-field>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col> </v-col>
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script setup>
+//
+</script>
+
+<template>
+  <v-card class="fill-height" title="Purchases and Sales">
+    <template #text>
+      <Bar :options="chartOptions" :data="chartData" />
+    </template>
+  </v-card>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,52 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="8" lg="4">
+        <p class="text-h5 font-weight-bold">Dashboard</p>
+        <p class="font-weight-light text-secondary">Welcome to budgetBuddy</p>
+      </v-col>
+      <v-col> </v-col>
+    </v-row>
+    <v-row>
+      <Card :dashCards="cards" />
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="12" lg="6">
+        <v-card title="Purchases and Sales">
+          <template #text>
+            <Chart />
+          </template>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-  //
+const cards = [
+  {
+    title: "Total Revenue",
+    amount: 435668,
+    difference: 5.2,
+    lastMonthAmount: 413200,
+  },
+  {
+    title: "Active Users",
+    amount: 1589,
+    difference: -3.4,
+    lastMonthAmount: 1645,
+  },
+  {
+    title: "New Sign-ups",
+    amount: 320,
+    difference: 12.1,
+    lastMonthAmount: 285,
+  },
+  {
+    title: "Customer Retention",
+    amount: 86,
+    difference: 2.5,
+    lastMonthAmount: 84,
+  },
+];
 </script>
